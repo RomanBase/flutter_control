2b59b4b40e658b1f78286a347364fe52dfe553b4
RomanBase <ro.hornak@gmail.com>
2019-08-11 16:41:18 +0200

    hotfix

--- lib/src/factory.dart
+++ lib/src/factory.dart
@@ -237,7 +237,7 @@ class ControlFactory implements Disposable {
     }
 
     if (includeFactory) {
-      return get()<T>() ?? defaultValue;
+      return get<T>() ?? defaultValue;
     }
 
     return defaultValue;
