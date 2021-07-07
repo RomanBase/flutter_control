5958c964306c47cfd14947857ad0f835b1b65059
RH <ro.hornak@gmail.com>
2021-07-07 16:28:46 +0200

    clients

--- lib/src/observable/control_observable.dart
+++ lib/src/observable/control_observable.dart
@@ -1,7 +1,10 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter_control/core.dart';
 
 class ControlObservable<T> implements Disposable {
   final subs = <ControlSubscription<T>>[];
+
+  dynamic data;
 
   bool _active = true;
 
@@ -18,6 +21,57 @@ class ControlObservable<T> implements Disposable {
   bool get isValid => true;
 
   bool get isActive => isValid && _active;
+
+  static ControlObservable<T> of<T>(dynamic object) {
+    if (object is ControlObservable<T>) {
+      return object;
+    } else if (object is Stream<T>) {
+      return ofStream(object);
+    } else if (object is Future<T>) {
+      return ofFuture(object);
+    } else if (object is Listenable) {
+      return ofListenable(object);
+    }
+
+    return ControlObservable<T>()..value = object;
+  }
+
+  static ControlObservable<T> ofStream<T>(Stream<T> stream) {
+    final observable = _ClientObservable<T>();
+    final sub = stream.listen((event) => observable.setValue(event));
+
+    observable.register(DisposableClient()..onDispose = () => sub.cancel());
+
+    return observable;
+  }
+
+  static ControlObservable<T> ofFuture<T>(Future<T> future) {
+    final observable = ControlObservable<T>();
+
+    future.then((value) => observable.setValue(value)).catchError((err) {
+      printDebug(err);
+    });
+
+    return observable;
+  }
+
+  static ControlObservable<T> ofListenable<T>(Listenable listenable) {
+    final observable = _ClientObservable<T>();
+
+    final callback = () {
+      if (listenable is ValueListenable<T>) {
+        observable.setValue(listenable.value);
+      } else {
+        observable.notify();
+      }
+    };
+
+    listenable.addListener(callback);
+    observable.register(DisposableClient()
+      ..onDispose = () => listenable.removeListener(callback));
+
+    return observable;
+  }
 
   ControlSubscription<T> subscribe(
     ValueCallback<T> action, {
@@ -74,5 +128,21 @@ class ControlObservable<T> implements Disposable {
   void dispose() {
     subs.forEach((element) => element.invalidate());
     subs.clear();
+  }
+}
+
+class _ClientObservable<T> extends ControlObservable<T> {
+  Disposable? _disposable;
+
+  void register(Disposable object) {
+    _disposable = object;
+  }
+
+  @override
+  void dispose() {
+    _disposable?.dispose();
+    _disposable = null;
+
+    super.dispose();
   }
 }
