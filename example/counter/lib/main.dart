9a26b293fb7e48a626878acdaf86ebdd087ff8c0
RomanBase <ro.hornak@gmail.com>
2023-02-13 15:49:46 +0100

    2.6.1

--- example/counter/lib/main.dart
+++ example/counter/lib/main.dart
@@ -10,7 +10,7 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    Control.init();
+    Control.initControl();
 
     return MaterialApp(
       title: 'Flutter Demo',
